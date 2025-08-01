8938f0b4fd98aa68946c0ef9589c191b4cb87d42
GulinAlexey <gulin-2001@mail.ru>
2025-08-01 16:53:55 +0700

    Заготовка проекта для сборки через cmake

---
+++ Transport_Info.cpp
@@ -0,0 +1,12 @@
+﻿// Transport_Info.cpp: определяет точку входа для приложения.
+//
+
+#include "Transport_Info.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

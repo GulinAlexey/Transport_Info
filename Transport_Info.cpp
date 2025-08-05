9d4d8341f006a2763ac6b7533b1a4f6f50185dad
GulinAlexey <gulin-2001@mail.ru>
2025-08-05 19:35:35 +0700

    Доб. проверка, что аргументы программы содержат только числа; доб. проверка на отсутствие аргументов при запуске

--- Transport_Info.cpp
+++ Transport_Info.cpp
@@ -5,21 +5,41 @@
 
 using namespace std;
 
+bool onlyNumInStrCheck(char* str); // Проверка, что в строке char* только символы чисел (в том числе без "-")
+
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "Rus"); // для корректного отображения русских букв
-	for (int i=1; i<argc; i++)
+	if (argc == 1)
 	{
-		Transport* transport = TransportFactory::create(atoi(argv[i]));
-		if (transport)
+		cout << "Для работы программы передайте ей числовые аргументы при запуске." << endl;
+		return 0;
+	}
+	for (int i=1; i<argc; i++) // перебрать переданные программе аргументы
+	{
+		Transport* transport = TransportFactory::create(atoi(argv[i])); // создать объект соответствующего класса в фабрике
+		if (!onlyNumInStrCheck(argv[i])) // проверка, что данный аргумент содержит только числа
+			transport = NULL;
+		if (transport) // если номер типа транспорта корректен, вывести инфо о нём
 		{
 			transport->showInfo();
-			delete transport; // Очистить память
+			delete transport; // очистить память
 		}
 		else
 			cout << "Неизвестный тип транспорта: " << argv[i] << endl;
 		cout << endl;
 	}
-
 	return 0;
+}
+
+bool onlyNumInStrCheck(char* str) // Проверка, что в строке char* только символы чисел (в том числе без "-")
+{
+	if (str == NULL || *str == '\0') // проверка на пустую строку
+		return false;
+	for (int i = 0; str[i] != '\0'; i++) // проверка, что в строке только числа
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return false;
+	}
+	return true;
 }
